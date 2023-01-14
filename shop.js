ac3e21d0659bc78a06c8dddc01fab73416a306e4
Piwormhole <122323197+Piwormhole@users.noreply.github.com>
2023-01-14 10:46:29 +0000

    Update shop.js

--- shop.js
+++ shop.js
@@ -3,8 +3,8 @@ const express = require ('express');
 const app = express();
 const fetch = require ('node-fetch')
 const { response } = require('express');
-const port =  [insert port number here];
-
+const env = require ('dotenv').config()
+const port =  process.env.PORT || 8050;
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.header(
@@ -13,12 +13,22 @@ app.use((req, res, next) => {
     );
     next();
   });
-app.use(express.static("public"));
+  app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-res.status(200).sendFile(__dirname + "/public/basket.html");
+res.status(200).sendFile(__dirname + "/public/shop.html");
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
 app.timeout = 2000;
+
 app.use(express.json());
+
+const myKey = process.env.myKey
+app.post('/store', async (request, response) => {
+    let data = request.body;
+  //do something with data such as store in shop's Database etc 
+    
+    //response.json(data)
+     })
