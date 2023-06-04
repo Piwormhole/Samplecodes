4be694e50c158d2e8dde46b3c2891926d1fe2855
Wormhole <122323197+Wormhole-Pi@users.noreply.github.com>
2023-06-04 12:06:23 +0100

    Update sdk.js

--- sdk.js
+++ sdk.js
@@ -35,7 +35,7 @@ class PostMessage{
                                             method: 'POST',
                                             headers: {
                                               //'Access-Control-Allow-Origin': '*',
-                                              'Authorization' : 'Bearer ktrryyytoken',
+                                              'Authorization' : 'ktrryyytoken',
                                               'Content-Type': 'application/json'
 
                                                       },
